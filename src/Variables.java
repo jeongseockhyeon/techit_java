05256532ae63e1383e7ffbf3dab604831dd942be
jsh <jeong9260@naver.com>
2023-11-23 18:53:47 +0900

    [ADD]Variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int num = 1;
+        double width = 12.34;
+        String content = "Programming";
+
+        //num = 10;
+        System.out.println(num);
+        System.out.println(width);
+        System.out.println(content);
+
+
+    }
+
+
+}
