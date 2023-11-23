ef5faada522790da40688f8fc424ce3f0a2adb3b
jsh <jeong9260@naver.com>
2023-11-23 19:47:24 +0900

    [ADD]ReferenceType

---
+++ src/ReferenceType.java
@@ -0,0 +1,21 @@
+public class ReferenceType {
+    public static void main(String[] args) {
+
+        // 참조형
+        // 참조형 데이터의 값 => 힙 메모리 영역
+        //변수에 대입되는 값 => 힙 메모리 영역의 주소값
+        //String a = new String("Hello!"); // a 자체가 Hello 값을 가지는 것은 아님
+//        System.out.println(a);
+
+//        String b = "Hello!"; //값 자체 대입
+//        String c = "Hello!"; //동일한 값이 있을 경우 기존 주소를 가르킴
+
+        // 주소 비교
+//        if (b ==  c ){
+//            System.out.println(true);
+//        } else {
+//            System.out.println(false);
+//        }
+
+    }
+}
