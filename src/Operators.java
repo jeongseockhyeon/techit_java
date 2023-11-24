0d605f17083771d106bb404b8e23d4bbf1a0bd24
jsh <jeong9260@naver.com>
2023-11-24 22:51:58 +0900

    [ADD]ConsoleIO

---
+++ src/Operators.java
@@ -0,0 +1,71 @@
+public class Operators {
+    public static void main(String[] args) {
+        // 산술 연산자
+
+//        int a = 10;
+//        int b = 20;
+//        String c = "abc";
+//
+//        System.out.println(a + c);
+//
+//        double d = 10;
+//        double e = 20;
+//
+//        System.out.println(d / e);
+//
+//        System.out.println(a + b);
+//        System.out.println(a - b);
+//        System.out.println(a * b);
+//        System.out.println(a / b);
+//        System.out.println(a % b);
+
+
+        // 비교 연산자
+
+//        int a = 5;
+//        int b = 8;
+//
+//        System.out.println(a > b);
+//        System.out.println(a < b);
+//
+//        System.out.println(a >= b);
+//        System.out.println(a <= b);
+
+        // = 대입연산자
+//        System.out.println(a == b);
+//        System.out.println(a != b);
+
+        // 기본형 데이터는 == 로 비교할 수 있지만 참조 데이터는 ==를 사용하면 힙메모리의 주소를 비교하게 됨으로 eqauls()를 사용하여야 한다.
+
+        // 논리 연산자
+
+//        int a = 3;
+//        int b = 3;
+//        int c = 5;
+
+        //AND(&&)
+//        System.out.println(c > a && c > b);
+
+        // OR(||)
+//        System.out.println(c > a || c < b);
+
+        // NOT(!)
+//        System.out.println(!true);
+//        System.out.println(!false);
+
+        // 대입 연산자
+
+        int a = 1;
+
+        // 증감 연산자
+        // 먼저 a를 호출 후 1를 더하거나 뺌
+        a++; // a = a + 1
+        a--; // a = a - 1
+
+        // a에 먼저 1를 더하거나 뺀 뒤 호출
+        ++a;
+        --a;
+
+
+    }
+}
