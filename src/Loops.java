f8dd0622031085ccaed039dcbe0e355252e967ab
jsh <jeong9260@naver.com>
2023-12-19 15:59:29 +0900

    [ADD]Loops

---
+++ src/Loops.java
@@ -0,0 +1,24 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        //반복문
+        //1. for
+        //초기화된 변수, 조건식, 증감식
+
+//        for (int i = 0; i < 10 ; i++){
+//            System.out.println(i);
+//        }
+
+        //2. while
+
+        int  b = 10;
+//        while (b > 0){
+//            System.out.println(b);
+//            b--;
+//        }
+        do {
+            System.out.println(b);
+            b--;
+        } while (b>0);
+    }
+}
